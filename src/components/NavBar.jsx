3e53ba8dddd5dad097a6639ec143ecc7c5ca64f8
Malte <jm.lindemann@web.de>
2024-08-26 22:17:53 +0200

    changed font color; set navbar styles;

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,16 +5,19 @@ import "../styles/css/navbar.css";
 
 function NavBar() {
   return (
-    <div className="navbar--wrapper">
+    <div className="navbar">
       <Link to="about" spy={true} smooth={true} duration={250}>
         About
       </Link>
+      <div className="width-md"></div>
       <Link to="projects" spy={true} smooth={true} duration={250}>
         Projects
       </Link>
+      <div className="width-md"></div>
       <Link to="career" spy={true} smooth={true} duration={250}>
         Career
       </Link>
+      <div className="width-md"></div>
       <Link to="contact" spy={true} smooth={true} duration={250}>
         Contact
       </Link>
