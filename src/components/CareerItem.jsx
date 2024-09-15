776d2318b28884931ff86f2c0a5f35dac47be98d
Malte <jm.lindemann@web.de>
2024-09-15 23:06:41 +0200

    added CareerImte with styles; edited career data

--- src/components/CareerItem.jsx
+++ src/components/CareerItem.jsx
@@ -1,8 +1,45 @@
 // Import Styles
 import "../styles/css/careeritem.css";
 
+// Import Images
+import isg from "../assets/images/isg.svg";
+import jpaule from "../assets/images/jpaule.png";
+import iff from "../assets/images/iff.png";
+
+const imageMap = {
+  isg: isg,
+  jpaule: jpaule,
+  iff: iff,
+};
+
 function CareerItem({ careerItem }) {
-  return <div className="career-item">{careerItem.company}</div>;
+  return (
+    <div className="career-item">
+      <div className="career-item--image">
+        <img
+          src={imageMap[careerItem.image]}
+          aria-label={careerItem.company}
+          alt={careerItem.company}
+        ></img>
+      </div>
+      <div className="career-item--description">
+        <div className="career-item--description--title">
+          {careerItem.title}
+        </div>
+        <div className="career-item--description--company">
+          {careerItem.company}
+        </div>
+        <div className="career-item--description--date">{careerItem.date}</div>
+        <div className="career-item--description--work">
+          <ul>
+            {careerItem.work.map((work) => (
+              <li>{work}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CareerItem;
