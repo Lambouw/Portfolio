f61382f56bd26de5866061687b6c495e8dbecd53
Malte <jm.lindemann@web.de>
2024-10-19 14:32:59 +0200

    finish Data.jsx; add padding to bottom of imprint;

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -16,7 +16,7 @@ function Data() {
         <div className="height-s"></div>
         <p className="data--container--header">Datenschutz</p>
         <p className="data--container--text">
-          Die Betreiberin dieser Seiten nimmt den Schutz Ihrer persönlichen
+          Der Betreiber dieser Seiten nimmt den Schutz Ihrer persönlichen
           Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten
           vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften
           sowie dieser Datenschutzerklärung.
@@ -39,15 +39,15 @@ function Data() {
           Website ist:
         </p>
         <div className="height-xs"></div>
-        <p className="data--container--text">Maria Musterfrau</p>
-        <p className="data--container--text">Loeweweg 1</p>
+        <p className="data--container--text">Malte Lindemann</p>
+        <p className="data--container--text">Loeweweg 3</p>
         <p className="data--container--text">71397 Leutenbach</p>
         <p className="data--container--text">Deutschland</p>
         <div className="height-xs"></div>
         <p className="data--container--text">
           E-Mail:&nbsp;
-          <a className="extern" href="mailto:kunst.lindemann@gmail.com">
-            kunst.lindemann@gmail.com
+          <a className="extern" href="mailto:jm.lindemann@web.de">
+            jm.lindemann@web.de
           </a>
         </p>
         <div className="height-xs"></div>
@@ -57,7 +57,8 @@ function Data() {
           Verarbeitung von personenbezogenen Daten (z.B. Namen, E-Mail-Adressen
           o. Ä.) entscheidet.
         </p>
-        <p className="data--container--header-small">
+        <div className="height-s"></div>
+        <p className="data--container--header">
           Widerruf Ihrer Einwilligung zur Datenverarbeitung
         </p>
         <p className="data--container--text">
@@ -67,23 +68,27 @@ function Data() {
           an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten
           Datenverarbeitung bleibt vom Widerruf unberührt.
         </p>
-        <p className="data--container--header-small">
+        <div className="height-s"></div>
+        <p className="data--container--header">
           Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde
         </p>
         <p className="data--container--text">
           Im Falle datenschutzrechtlicher Verstöße steht dem Betroffenen ein
           Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
         </p>
-        <h2 className="data--container--header">
+        <div className="height-m"></div>
+        <h3 className="data--container--header">
           2. Datenerfassung auf dieser Website
-        </h2>
-        <p className="data--container--header-small">Server-Log-Dateien</p>
+        </h3>
+        <div className="height-s"></div>
+        <p className="data--container--header">Server-Log-Dateien</p>
         <p className="data--container--text">
           Der Provider dieser Seiten (Strato AG, Pascalstraße 10, 10587 Berlin,
           Deutschland) erhebt und speichert automatisch Informationen in
           sogenannten Server-Log-Dateien, die Ihr Browser automatisch an uns
           übermittelt. Dies sind:
         </p>
+        <div className="height-xs"></div>
         <p className="data--container--list">
           <ul>
             <li>Browsertyp und Browserversion</li>
@@ -94,6 +99,7 @@ function Data() {
             <li>IP-Adresse</li>
           </ul>
         </p>
+        <div className="height-xs"></div>
         <p className="data--container--text">
           Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht
           vorgenommen.
@@ -105,7 +111,8 @@ function Data() {
           der technisch fehlerfreien Darstellung und der Optimierung seiner
           Website – hierzu müssen die Server-Log-Files erfasst werden.
         </p>
-        <p className="data--container--header-small">Kontaktformular</p>
+        <div className="height-s"></div>
+        <p className="data--container--header">Kontaktformular</p>
         <p className="data--container--text">
           Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre
           Angaben aus dem Anfrageformular inklusive der von Ihnen dort
@@ -132,7 +139,8 @@ function Data() {
           Zwingende gesetzliche Bestimmungen – insbesondere Aufbewahrungsfristen
           – bleiben unberührt.
         </p>
-        <h2 className="data--container--header">3. Hosting bei Strato</h2>
+        <div className="height-m"></div>
+        <h3>3. Hosting bei Strato</h3>
         <p className="data--container--text">
           Diese Website wird bei Strato gehostet. Der Anbieter ist die Strato
           AG, Pascalstraße 10, 10587 Berlin, Deutschland. Wenn Sie diese Website
@@ -154,9 +162,8 @@ function Data() {
           abgeschlossen, um sicherzustellen, dass die Datenverarbeitung im
           Einklang mit den Datenschutzbestimmungen erfolgt.
         </p>
-        <h2 className="data--container--header">
-          4. SSL- bzw. TLS-Verschlüsselung
-        </h2>
+        <div className="height-m"></div>
+        <h3>4. SSL- bzw. TLS-Verschlüsselung</h3>
         <p className="data--container--text">
           Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der
           Übertragung vertraulicher Inhalte, wie zum Beispiel Anfragen, die Sie
