600f00d135fb231ecbe5afda35f043afa0e964f8
Malte <jm.lindemann@web.de>
2024-12-01 11:14:28 +0100

    FIX: Mail Button in Contact section;

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -96,12 +96,9 @@ function Contact() {
           </div>
           <div className="contact--container--addresses--spacer"></div>
           <div className="contact--container--addresses--socials">
-            <Link
+            <a
               className="contact--container--addresses--socials--icons--icon"
-              to="contact"
-              spy={true}
-              smooth={true}
-              duration={250}
+              href="mailto:jm.lindemann@web.de"
             >
               <svg
                 stroke="currentColor"
@@ -113,7 +110,7 @@ function Contact() {
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
               </svg>
-            </Link>
+            </a>
             <div className="width-xs"></div>
             <div className="width-xs"></div>
             <a
