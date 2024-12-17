ed80fb963e122b3644e5b4dc168f068d832e5491
Malte <jm.lindemann@web.de>
2024-12-18 00:06:07 +0100

    FIX: link to twinstore; add log on main.jsx; add correct postman logo -> was showing a black out for all;

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+console.log('Hit me up! -> info@maltelindemann.com');
+console.log(':-)');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
