700bb87dedfe91b4aa79fac5736b3b69a2516fd8
Malte <jm.lindemann@web.de>
2024-12-28 14:08:44 +0100

    Add Shimmer Colors; Add Shimmer effect on image in ProjectCard;

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -20,21 +20,34 @@ const imageMap = {
 
 const ProjectCard = React.forwardRef(({ project }, ref) => {
   const [open, setOpen] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false); // Track image load state
 
   // Toggle dialog open state
   const toggleDialog = () => {
     setOpen(!open);
   };
 
+  const handleImageLoad = () => {
+    setImageLoaded(true); // Set image as loaded when it finishes loading
+  };
+
   return (
     <>
       <div className="project-card" ref={ref} onClick={toggleDialog}>
         <div className="project-card--upper">
+          {/* Shimmer placeholder */}
+          {!imageLoaded && <div className="project-card--upper--shimmer"></div>}
+
+          {/* Image */}
           <img
-            className="project-card--upper--image"
+            // className="project-card--upper--image"
+            className={`project-card--upper--image ${
+              imageLoaded ? "visible" : "hidden"
+            }`}
             aria-label={project.name}
             alt={project.name}
             src={imageMap[project.image]} // Get image from imageMap
+            onLoad={handleImageLoad} // Triggered when the image is loaded
           />
           <div className="height-s"></div>
           <h3 className="project-card--upper--name">{project.name}</h3>
